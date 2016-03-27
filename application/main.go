ff97e46920140a05161d1fdd113d606cdfab14f3
EC2 Default User <ec2-user@ip-172-31-4-241.us-west-2.compute.internal>
2016-03-27 03:29:15 +0000

    A bit more progress on the redis servers. Have to integrate better with AngularJS side.

--- application/main.go
+++ application/main.go
@@ -5,6 +5,7 @@ import (
   "io/ioutil"
   "net/http"
   "encoding/json"
+  "gopkg.in/redis.v3"
 )
 
 
@@ -38,21 +39,43 @@ func handler(w http.ResponseWriter, r *http.Request) {
   return
 }
 
-func redisHandler(w http.ResponseWriter, r *http.Request) {
-  value := r.URL.Query().Get("action")
+func RedisConnection() (connection *redis.Client, err error){
+  client := redis.NewClient(&redis.Options{
+    Addr: "172.17.0.3:6379",
+    Password: "",
+    DB: 0,
+  })
 
-  var text RedisItem
+  pong, err := client.Ping().Result()
+  fmt.Println("Redis connection test: ", pong, err)
+  return client, err
+}
+
+
+func redisHandler(w http.ResponseWriter, r *http.Request) {
+  action := r.URL.Query().Get("action")
+  redisClient,_ := RedisConnection()
+  //var text RedisItem
   items := []RedisItem{}
   collection := RedisCollection{items}
-  if value == "display" {
-    text = RedisItem{"remoteKey", "remoteData"}
-    collection.RedisItems = append(collection.RedisItems, text)
-    text = RedisItem{"remoteKey2", "remoteData2"}
-    collection.RedisItems = append(collection.RedisItems, text)
-  } else if value == "add" {
-    text = RedisItem{"remoteKey1", "remoteData"}
-  } else if value == "del" {
-    text = RedisItem{"remoteKey2", "remoteData"}
+  if action == "display" {
+    data := redisClient.Keys("*")
+    fmt.Println(data)
+  } else if action == "add" {
+    key := r.URL.Query().Get("key")
+    value := r.URL.Query().Get("value")
+    err := redisClient.Set(key, value, 0).Err()
+    if err != nil {
+      http.Error(w, "Error with adding key", http.StatusInternalServerError)
+      return
+    }
+  } else if action == "del" {
+    key := r.URL.Query().Get("key")
+    err := redisClient.Del(key).Err()
+    if err != nil {
+      http.Error(w, "Error with deleting key", http.StatusInternalServerError)
+      return
+    }
   } else {
     http.Error(w, "Unknown Error", http.StatusInternalServerError)
     return
@@ -66,7 +89,8 @@ func redisHandler(w http.ResponseWriter, r *http.Request) {
 
   w.Header().Set("Content-Type", "application/json")
   fmt.Fprintf(w, "%s", js)
-  fmt.Printf("redisAction: %s - IP: %s\n", value, r.RemoteAddr)
+  fmt.Printf("redisAction: %s - IP: %s\n", action, r.RemoteAddr)
+  redisClient.Close()
   return
 }
 
